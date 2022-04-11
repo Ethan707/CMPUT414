b1efc6f54ec0c3b8c1b03abfe591fb5cb6f8d469
Ethan707 <24852017+Ethan707@users.noreply.github.com>
2022-04-11 02:15:46 -0600

    Update model

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 '''
 Author: Ethan Chen
 Date: 2022-03-16 17:47:49
-LastEditTime: 2022-04-07 21:53:43
+LastEditTime: 2022-04-11 01:47:39
 LastEditors: Ethan Chen
 Description: The model class for the latent 3d model
 FilePath: /CMPUT414/src/model.py
@@ -102,154 +102,90 @@ class r_GAN_discriminator(nn.Module):
 # laplacian completion network
 
 
-# Point cloud Net
-# from learning3d
-# https://github.com/vinits5/learning3d
+class PCN(nn.Module):
+    """
+    "PCN: Point Cloud Completion Network"
+    (https://arxiv.org/pdf/1808.00671.pdf)
 
+    Attributes:
+        num_dense:  16384
+        latent_dim: 1024
+        grid_size:  4
+        num_coarse: 1024
+    """
 
-class PCN(torch.nn.Module):
-    def __init__(self, emb_dims=1024, input_shape="bnc", num_coarse=1024, grid_size=4, detailed_output=True):
-        # emb_dims:			Embedding Dimensions for PCN.
-        # input_shape:		Shape of Input Point Cloud (b: batch, n: no of points, c: channels)
-        super(PCN, self).__init__()
-        if input_shape not in ["bcn", "bnc"]:
-            raise ValueError("Allowed shapes are 'bcn' (batch * channels * num_in_points), 'bnc' ")
-        self.input_shape = input_shape
-        self.emb_dims = emb_dims
-        self.num_coarse = num_coarse
-        self.detailed_output = detailed_output
+    def __init__(self, num_dense=16384, latent_dim=1024, grid_size=4):
+        super().__init__()
+
+        self.num_dense = num_dense
+        self.latent_dim = latent_dim
         self.grid_size = grid_size
-        self.num_fine = self.grid_size ** 2 * self.num_coarse
-        self.pooling = nn.MaxPool1d
 
-        self.encoder()
-        self.decoder_layers = self.decoder()
-        if detailed_output:
-            self.folding_layers = self.folding()
+        assert self.num_dense % self.grid_size ** 2 == 0
 
-    def encoder_1(self):
-        self.conv1 = torch.nn.Conv1d(3, 128, 1)
-        self.conv2 = torch.nn.Conv1d(128, 256, 1)
-        self.relu = torch.nn.ReLU()
+        self.num_coarse = self.num_dense // (self.grid_size ** 2)
 
-        # self.bn1 = torch.nn.BatchNorm1d(128)
-        # self.bn2 = torch.nn.BatchNorm1d(256)
+        self.first_conv = nn.Sequential(
+            nn.Conv1d(3, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 256, 1)
+        )
 
-        layers = [self.conv1, self.relu, self.conv2]
-        return layers
+        self.second_conv = nn.Sequential(
+            nn.Conv1d(512, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, self.latent_dim, 1)
+        )
 
-    def encoder_2(self):
-        self.conv3 = torch.nn.Conv1d(2*256, 512, 1)
-        self.conv4 = torch.nn.Conv1d(512, self.emb_dims, 1)
+        self.mlp = nn.Sequential(
+            nn.Linear(self.latent_dim, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 3 * self.num_coarse)
+        )
 
-        # self.bn3 = torch.nn.BatchNorm1d(512)
-        # self.bn4 = torch.nn.BatchNorm1d(self.emb_dims)
-        self.relu = torch.nn.ReLU()
+        self.final_conv = nn.Sequential(
+            nn.Conv1d(1024 + 3 + 2, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, 3, 1)
+        )
+        a = torch.linspace(-0.05, 0.05, steps=self.grid_size, dtype=torch.float).view(1,
+                                                                                      self.grid_size).expand(self.grid_size, self.grid_size).reshape(1, -1)
+        b = torch.linspace(-0.05, 0.05, steps=self.grid_size, dtype=torch.float).view(self.grid_size,
+                                                                                      1).expand(self.grid_size, self.grid_size).reshape(1, -1)
 
-        layers = [self.conv3, self.relu,
-                  self.conv4]
-        return layers
+        self.folding_seed = torch.cat([a, b], dim=0).view(1, 2, self.grid_size ** 2).cuda()  # (1, 2, S)
 
-    def encoder(self):
-        self.encoder_layers1 = self.encoder_1()
-        self.encoder_layers2 = self.encoder_2()
+    def forward(self, xyz):
+        B, N, _ = xyz.shape
 
-    def decoder(self):
-        self.linear1 = torch.nn.Linear(self.emb_dims, 1024)
-        self.linear2 = torch.nn.Linear(1024, 1024)
-        self.linear3 = torch.nn.Linear(1024, self.num_coarse*3)
+        # encoder
+        feature = self.first_conv(xyz.transpose(2, 1))                                       # (B,  256, N)
+        feature_global = torch.max(feature, dim=2, keepdim=True)[0]                          # (B,  256, 1)
+        feature = torch.cat([feature_global.expand(-1, -1, N), feature], dim=1)              # (B,  512, N)
+        feature = self.second_conv(feature)                                                  # (B, 1024, N)
+        feature_global = torch.max(feature, dim=2, keepdim=False)[0]                           # (B, 1024)
 
-        # self.bn1 = torch.nn.BatchNorm1d(1024)
-        # self.bn2 = torch.nn.BatchNorm1d(1024)
-        # self.bn3 = torch.nn.BatchNorm1d(self.num_coarse*3)
-        self.relu = torch.nn.ReLU()
+        # decoder
+        # (B, num_coarse, 3), coarse point cloud
+        coarse = self.mlp(feature_global).reshape(-1, self.num_coarse, 3)
+        point_feat = coarse.unsqueeze(2).expand(-1, -1, self.grid_size ** 2, -1)             # (B, num_coarse, S, 3)
+        point_feat = point_feat.reshape(-1, self.num_dense, 3).transpose(2, 1)               # (B, 3, num_fine)
 
-        layers = [self.linear1, self.relu,
-                  self.linear2, self.relu,
-                  self.linear3]
-        return layers
+        seed = self.folding_seed.unsqueeze(2).expand(B, -1, self.num_coarse, -1)             # (B, 2, num_coarse, S)
+        seed = seed.reshape(B, -1, self.num_dense)                                           # (B, 2, num_fine)
 
-    def folding(self):
-        self.conv5 = torch.nn.Conv1d(1029, 512, 1)
-        self.conv6 = torch.nn.Conv1d(512, 512, 1)
-        self.conv7 = torch.nn.Conv1d(512, 3, 1)
+        feature_global = feature_global.unsqueeze(2).expand(-1, -1, self.num_dense)          # (B, 1024, num_fine)
+        feat = torch.cat([feature_global, seed, point_feat], dim=1)                          # (B, 1024+2+3, num_fine)
 
-        # self.bn5 = torch.nn.BatchNorm1d(512)
-        # self.bn6 = torch.nn.BatchNorm1d(512)
-        self.relu = torch.nn.ReLU()
+        # (B, 3, num_fine), fine point cloud
+        fine = self.final_conv(feat) + point_feat
 
-        layers = [self.conv5, self.relu,
-                  self.conv6, self.relu,
-                  self.conv7]
-        return layers
-
-    def fine_decoder(self):
-        # Fine Output
-        linspace = torch.linspace(-0.05, 0.05, steps=self.grid_size)
-        grid = torch.meshgrid(linspace, linspace)
-        grid = torch.reshape(torch.stack(grid, dim=2), (-1, 2))								# 16x2
-        grid = torch.unsqueeze(grid, dim=0)													# 1x16x2
-        grid_feature = grid.repeat([self.coarse_output.shape[0], self.num_coarse, 1])		# Bx16384x2
-
-        point_feature = torch.unsqueeze(self.coarse_output, dim=2)							# Bx1024x1x3
-        point_feature = point_feature.repeat([1, 1, self.grid_size ** 2, 1])				# Bx1024x16x3
-        point_feature = torch.reshape(point_feature, (-1, self.num_fine, 3))				# Bx16384x3
-
-        global_feature = torch.unsqueeze(self.global_feature_v, dim=1)						# Bx1x1024
-        global_feature = global_feature.repeat([1, self.num_fine, 1])						# Bx16384x1024
-
-        feature = torch.cat([grid_feature, point_feature, global_feature], dim=2)			# Bx16384x1029
-
-        center = torch.unsqueeze(self.coarse_output, dim=2)									# Bx1024x1x3
-        center = center.repeat([1, 1, self.grid_size ** 2, 1])								# Bx1024x16x3
-        center = torch.reshape(center, [-1, self.num_fine, 3])								# Bx16384x3
-
-        output = feature.permute(0, 2, 1)
-        for idx, layer in enumerate(self.folding_layers):
-            output = layer(output)
-        fine_output = output.permute(0, 2, 1) + center
-        return fine_output
-
-    def encode(self, input_data):
-        output = input_data
-        for idx, layer in enumerate(self.encoder_layers1):
-            output = layer(output)
-
-        global_feature_g = self.pooling(output)
-
-        global_feature_g = global_feature_g.unsqueeze(2)
-        global_feature_g = global_feature_g.repeat(1, 1, self.num_points)
-        output = torch.cat([output, global_feature_g], dim=1)
-
-        for idx, layer in enumerate(self.encoder_layers2):
-            output = layer(output)
-
-        self.global_feature_v = self.pooling(output)
-
-    def decode(self):
-        output = self.global_feature_v
-        for idx, layer in enumerate(self.decoder_layers):
-            output = layer(output)
-        self.coarse_output = output.view(self.global_feature_v.shape[0], self.num_coarse, 3)
-
-    def forward(self, input_data):
-        # input_data: 		Point Cloud having shape input_shape.
-        # output:			PointNet features (Batch x emb_dims)
-        if self.input_shape == "bnc":
-            self.num_points = input_data.shape[1]
-            input_data = input_data.permute(0, 2, 1)
-        else:
-            self.num_points = input_data.shape[2]
-        if input_data.shape[1] != 3:
-            raise RuntimeError("shape of x must be of [Batch x 3 x NumInPoints]")
-
-        self.encode(input_data)
-        self.decode()
-
-        result = {'coarse_output': self.coarse_output}
-
-        if self.detailed_output:
-            fine_output = self.fine_decoder()
-            result['fine_output'] = fine_output
-
-        return result
+        return coarse.contiguous(), fine.transpose(1, 2).contiguous()
--- src/train.py
+++ src/train.py
@@ -1,11 +1,16 @@
 '''
 Author: Ethan Chen
 Date: 2022-03-16 17:47:49
-LastEditTime: 2022-04-09 17:38:03
+LastEditTime: 2022-04-11 02:09:19
 LastEditors: Ethan Chen
-Description: 
+Description:
 FilePath: /CMPUT414/src/train.py
 '''
+from loss import *
+from torchsummary import summary
+from pytorch3d.loss import chamfer_distance
+from model import *
+from data import *
 import argparse
 import os
 import random
@@ -14,11 +19,6 @@ import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from data import *
-from model import *
-from pytorch3d.loss import chamfer_distance
-from torchsummary import summary
-from loss import *
 # TODO:
 # 1. chamer_distance + classification loss
 # 2. add the laplace GAN
@@ -28,68 +28,147 @@ from loss import *
 # 5. train with different batch size
 # 6. train with different learning rate
 # 7. train with different epoch
+TRAIN_DATA_PATH = '/home/ethan/Code/1/Project/CMPUT414/data/modelnet40_ply_hdf5_2048/train_files.txt'
+TEST_DATA_PATH = '/home/ethan/Code/1/Project/CMPUT414/data/modelnet40_ply_hdf5_2048/test_files.txt'
+LOSS_MODEL_PATH_2048 = '/home/ethan/Code/1/Project/CMPUT414/model/loss_network_2048.pth'
+LOSS_MODEL_PATH_128 = '/home/ethan/Code/1/Project/CMPUT414/model/loss_network_128.pth'
+COMPLETION_MODEL_PATH = '/home/ethan/Code/1/Project/CMPUT414/model/completion_network.pth'
 
 
-def train_loss_network(model):
-    num_epoch = 1001
-    modelnet40_train = ModelNet40(partition='train', num_points=2048,
-                                  data_path='/home/ethan/Code/1/Project/CMPUT414/data/modelnet40_ply_hdf5_2048/train_files.txt')
-    modelnet40_test = ModelNet40(partition='test', num_points=2048,
-                                 data_path='/home/ethan/Code/1/Project/CMPUT414/data/modelnet40_ply_hdf5_2048/test_files.txt')
-    train_loader = DataLoader(modelnet40_train,
-                              num_workers=8, batch_size=100, shuffle=True, drop_last=True)
-    test_loader = DataLoader(modelnet40_test, num_workers=8, batch_size=100, shuffle=True, drop_last=True)
+def train_loss_network(model, path, num_epoch=100, num_points=2048, device='cuda'):
+    modelnet40_train = ModelNet40(num_points=num_points, data_path=TRAIN_DATA_PATH)
+    modelnet40_test = ModelNet40(num_points=num_points, data_path=TEST_DATA_PATH)
+    train_loader = DataLoader(modelnet40_train, num_workers=8, batch_size=32, shuffle=True, drop_last=True)
+    test_loader = DataLoader(modelnet40_test, num_workers=8, batch_size=32, shuffle=True, drop_last=True)
 
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
     criterion = nn.CrossEntropyLoss()
 
+    best_acc = 0
+    best_epoch = 0
     for epoch in range(num_epoch):
         train_loss = 0
-        correct = 0
+        train_correct = 0
         model.train()
         for i, data in enumerate(tqdm(train_loader)):
-            ground_truth, (cutout_pointcloud, translatedCloud, noiseCloud, rotatedCloud), label = data
-            # ground_truth train
-            ground_truth = ground_truth.to(device).permute(0, 2, 1)
+            original, translated, rotated, label = data
+            ground_truth = original[0].to(device)
             label = label.to(device).squeeze()
             optimizer.zero_grad()
-            output, trans, trans_feat = model(ground_truth)
+            output, trans, trans_feat, feature_map = model(ground_truth)
             loss = criterion(output, label)
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
             pred = output.argmax(dim=1)
-            correct += (pred == label).sum().item()
+            train_correct += (pred == label).sum().item()
+        scheduler.step()
+        model.eval()
+        test_loss = 0
+        test_correct = 0
+        with torch.no_grad():
+            for i, data in enumerate(tqdm(test_loader)):
+                original, translated, rotated, label = data
+                ground_truth = original[0].to(device)
+                label = label.to(device).squeeze()
+                output, trans, trans_feat, feature_map = model(ground_truth)
 
-        if epoch % 10 == 0:
-            print("Train epoch: {}, loss: {}, accuracy: {}".format(
-                epoch, train_loss/len(train_loader.dataset), correct / len(train_loader.dataset)))
+                loss = criterion(output, label)
+                test_loss += loss.item()
+                pred = output.argmax(dim=1)
+                test_correct += (pred == label).sum().item()
 
-            model.eval()
-            test_loss = 0
-            test_correct = 0
-            with torch.no_grad():
-                for i, data in enumerate(tqdm(test_loader)):
-                    ground_truth, label = data
-                    ground_truth = ground_truth.to(device).permute(0, 2, 1)
-                    label = label.to(device).squeeze()
-                    output, trans, trans_feat = model(ground_truth)
+        print("epoch: {}, train loss: {}, train accuracy: {}, test loss: {}, test accuracy: {}".format(
+            epoch, train_loss/len(train_loader.dataset),
+            train_correct / len(train_loader.dataset),
+            test_loss / len(test_loader.dataset),
+            test_correct / len(test_loader.dataset)))
 
-                    loss = criterion(output, label)
-                    test_loss += loss.item()
-                    pred = output.argmax(dim=1)
-                    test_correct += (pred == label).sum().item()
-            print("Test epoch: {}, loss: {}, accuracy: {}".format(epoch, test_loss /
-                  len(test_loader.dataset), test_correct / len(test_loader.dataset)))
-            torch.save(model.state_dict(), '/home/ethan/Code/1/Project/CMPUT414/model/cls_model_epoch_{}.pth'.format(epoch))
+        if test_correct / len(test_loader.dataset) > best_acc:
+            best_acc = test_correct / len(test_loader.dataset)
+            best_epoch = epoch
+            torch.save(model.state_dict(), path)
+
+    print("Best accuracy: {}, at epoch: {}".format(best_acc, best_epoch))
+
+
+def train_completion_network(model, loss_model, path, num_epoch=100, num_points=2048, device='cuda'):
+    modelnet40_train = ModelNet40(num_points=num_points, data_path=TRAIN_DATA_PATH)
+    modelnet40_test = ModelNet40(num_points=num_points, data_path=TEST_DATA_PATH)
+    train_loader = DataLoader(modelnet40_train, num_workers=8, batch_size=32, shuffle=True, drop_last=True)
+    test_loader = DataLoader(modelnet40_test, num_workers=8, batch_size=32, shuffle=True, drop_last=True)
+
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999))
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+
+    best_acc = 0
+    best_epoch = 0
+    for epoch in range(num_epoch):
+        train_loss = 0
+        train_correct = 0
+        model.train()
+        for i, data in enumerate(tqdm(train_loader)):
+            original, translated, rotated, label = data
+            cutout = original[0].to(device)
+            label = label.to(device).squeeze()
+            optimizer.zero_grad()
+            coarse_pred, dense_pred = model(cutout)
+            print(coarse_pred.shape, dense_pred.shape)
+            output, trans, trans_feat, feature_map = loss_model(dense_pred)
+            print(feature_map.shape)
+
+
+def modeltest(model):
+    pass
 
 
 if __name__ == "__main__":
     random.seed(1234)
     torch.manual_seed(1234)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    loss_network = PointNetCls(k=40)
-    # loss_network = PointNet(1024, 40)
+    torch.cuda.manual_seed(1234)
+    torch.cuda.manual_seed_all(1234)
 
-    train_loss_network(loss_network)
+    # model
+    loss_network = PointNetCls(k=40)
+    loss_network_128 = PointNetCls(k=40)
+    completion_network = PCN(num_dense=2048)
+
+    # arguments
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    num_epoch = 100
+    # load & train model
+    if not os.path.exists(LOSS_MODEL_PATH_2048):
+        train_loss_network(loss_network, path=LOSS_MODEL_PATH_2048, num_epoch=num_epoch, num_points=2048, device=device)
+    loss_network.load_state_dict(torch.load(LOSS_MODEL_PATH_2048))
+    loss_network.to(device)
+
+    train_loss_network(loss_network_128, path=LOSS_MODEL_PATH_128, num_epoch=num_epoch, num_points=128, device=device)
+
+    # train_completion_network(completion_network, path=None, loss_model=loss_network, num_epoch=num_epoch, device=device)
+
+    # modeltest(completion_network)
+
+    # model_dict = torch.load('/home/ethan/Code/1/Project/CMPUT414/model/cls_model_epoch_260.pth')
+    # loss_network.load_state_dict(model_dict)
+    # loss_network.cuda()
+    # loss_network.eval()
+    # summary(loss_network, (3, 2048))
+    # te.list_module_names(loss_network)
+    # for name, module in loss_network.named_modules():
+    #     print(name)
+    # dummy_input = torch.randn(1, 3, 2048).cuda()
+    # model = te.Extractor(loss_network, ["feat.stn.conv1"])
+    # model_output, features = model(dummy_input)
+    # print({name: feature.shape for name, feature in features.items()})
+    # test_feature = loss_network.features
+    # test_feature.eval()
+    # loss_network = ...
+
+    # if use_pretrained_completion:
+    #     model_dict = torch.load('/home/ethan/Code/1/Project/CMPUT414/model/com_model_epoch_260.pth')
+    #     completion_network.load_state_dict(model_dict)
+    # else:
+    #     train_completion_network(completion_network, loss_network)
