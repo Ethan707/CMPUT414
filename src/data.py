a5a7f3e49c7690903881e358b1b5886138073cca
Ethan707 <24852017+Ethan707@users.noreply.github.com>
2022-04-12 19:32:09 -0600

    add evaluation

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 '''
 Author: Yuxi Chen
 Date: 2022-03-15 17:49:50
-LastEditTime: 2022-04-11 18:08:24
+LastEditTime: 2022-04-12 19:15:59
 LastEditors: Ethan Chen
 Description:
 FilePath: /CMPUT414/src/data.py
@@ -14,47 +14,49 @@ import h5py
 from util import *
 import open3d as o3d
 
-SHAPE = ["airplane",
-         "bathtub",
-         "bed",
-         "bench",
-         "bookshelf",
-         "bottle",
-         "bowl",
-         "car",
-         "chair",
-         "cone",
-         "cup",
-         "curtain",
-         "desk",
-         "door",
-         "dresser",
-         "flower_pot",
-         "glass_box",
-         "guitar",
-         "keyboard",
-         "lamp",
-         "laptop",
-         "mantel",
-         "monitor",
-         "night_stand",
-         "person",
-         "piano",
-         "plant",
-         "radio",
-         "range_hood",
-         "sink",
-         "sofa",
-         "stairs",
-         "stool",
-         "table",
-         "tent",
-         "toilet",
-         "tv_stand",
-         "vase",
-         "wardrobe",
-         "xbox",
-         ]
+TRAIN_DATA_PATH = '/home/ethan/Code/Project/CMPUT414/data/modelnet40_ply_hdf5_2048/train_files.txt'
+TEST_DATA_PATH = '/home/ethan/Code/Project/CMPUT414/data/modelnet40_ply_hdf5_2048/test_files.txt'
+SHAPE_NAME = ["airplane",
+              "bathtub",
+              "bed",
+              "bench",
+              "bookshelf",
+              "bottle",
+              "bowl",
+              "car",
+              "chair",
+              "cone",
+              "cup",
+              "curtain",
+              "desk",
+              "door",
+              "dresser",
+              "flower_pot",
+              "glass_box",
+              "guitar",
+              "keyboard",
+              "lamp",
+              "laptop",
+              "mantel",
+              "monitor",
+              "night_stand",
+              "person",
+              "piano",
+              "plant",
+              "radio",
+              "range_hood",
+              "sink",
+              "sofa",
+              "stairs",
+              "stool",
+              "table",
+              "tent",
+              "toilet",
+              "tv_stand",
+              "vase",
+              "wardrobe",
+              "xbox",
+              ]
 
 
 def download():
@@ -70,7 +72,7 @@ def download():
         os.system('rm %s' % (zipfile))
 
 
-def load_data(data_path):
+def load_data(data_path, shape=-1):
     download()
     all_data = []
     all_label = []
@@ -80,8 +82,16 @@ def load_data(data_path):
             data = f['data'][:].astype('float32')
             label = f['label'][:].astype('int64')
             f.close()
-            all_data.append(data)
-            all_label.append(label)
+            if shape <= 39 and shape >= 0:
+                shape_index = np.where(label == shape)
+                all_data.append(data[shape_index[0], :, :])
+                all_label.append(label[shape_index[0], :])
+            elif shape == -1:
+                all_data.append(data)
+                all_label.append(label)
+            else:
+                print("shape not match, shape should be in [0, 39] or -1 for all shapes")
+                assert False
     all_data = np.concatenate(all_data, axis=0)
     all_label = np.concatenate(all_label, axis=0)
     return all_data, all_label
@@ -124,9 +134,9 @@ def plot_point_cloud_one_view(pointclouds, file_name, titles, suptitle='', sizes
 
 
 class ModelNet40(Dataset):
-    def __init__(self, num_points, data_path, num_coarse=128, severity=1):
+    def __init__(self, num_points, data_path, num_coarse=128, severity=1, shape=-1):
         super().__init__()
-        self.data, self.label = load_data(data_path)
+        self.data, self.label = load_data(data_path, shape)
         self.num_points = num_points
         self.severity = severity
         self.num_coarse = num_coarse
@@ -156,7 +166,6 @@ class ModelNet40(Dataset):
         np.random.shuffle(cutoutCloud)
         np.random.shuffle(translatedCutout)
         np.random.shuffle(rotatedCutout)
-        assert coarseCloud.shape == translatedCoarse.shape == rotatedCoarse.shape
 
         original = (pointCloud, coarseCloud, cutoutCloud)
         translated = (translatedCloud, translatedCoarse, translatedCutout)
@@ -171,31 +180,32 @@ class ModelNet40(Dataset):
 # at most 2048 points for each sample
 # total 9840 samples
 # total 40 classes
-if __name__ == '__main__':
-    train = ModelNet40(1024, 'data/modelnet40_ply_hdf5_2048/train_files.txt', severity=5)
+if __name__ == "__main__":
+    s = ModelNet40(2048, TRAIN_DATA_PATH, shape=0)
+    # w = ModelNet40(2048, TEST_DATA_PATH, shape="airplane")
+    # category_train = {}
+    # category_test = {}
 
-    # print("Translated:", example_data_translated.shape)
-    # print("Noise:", example_data_noise.shape)
-    # print("Rotated:", example_data_rotated.shape)
-    # print("Label", example_label)
+    # for i in range(len(s)):
+    #     original, translated, rotated, label = s[i]
+    #     if label[0] not in category_train:
+    #         category_train[label[0]] = 0
+    #     category_train[label[0]] += 1
 
-    original, translated, rotated, label = train[0]
-    a0, a1, a2 = original
-    b0, b1, b2 = translated
-    c0, c1, c2 = rotated
+    # for i in range(len(w)):
+    #     original, translated, rotated, label = w[i]
+    #     if label[0] not in category_test:
+    #         category_test[label[0]] = 0
+    #     category_test[label[0]] += 1
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.scatter(a1[:, 0], a1[:, 1], a1[:, 2], '.')
-    ax.scatter(b1[:, 0], b1[:, 1], b1[:, 2], '.')
-    ax.scatter(c1[:, 0], c1[:, 1], c1[:, 2], '.')
-    # ax.scatter(b[:, 0], b[:, 1], b[:, 2], '.', color='r', label='gt')
+    # print(len(s))
+    # print(len(w))
+    # print(category_train)
+    # print(category_test)
 
-    plt.show()
-    # label_set = {}
-    # for _, data, label in test:
-    #     assert len(label) == 1
-    #     if label[0] not in label_set:
-    #         label_set[label[0]] = 0
-    #     label_set[label[0]] += 1
-    # print(label_set)
+# 9840
+# 2468
+# {30: 680, 27: 104, 29: 128, 22: 465, 7: 197, 28: 115, 0: 625, 20: 149, 35: 344, 32: 90, 34: 163, 26: 239, 5: 335, 12: 200, 10: 79, 21: 284, 14: 200, 36: 267, 2: 515, 33: 392,
+#     24: 88, 9: 167, 4: 572, 31: 124, 18: 145, 11: 137, 16: 171, 17: 155, 8: 889, 25: 231, 37: 475, 23: 200, 15: 149, 6: 64, 3: 173, 38: 87, 1: 106, 13: 109, 19: 124, 39: 103}
+# {4: 100, 0: 100, 2: 100, 8: 100, 23: 86, 37: 100, 35: 100, 28: 100, 16: 100, 20: 20, 33: 100, 30: 100, 26: 100, 1: 50, 17: 100, 22: 100, 21: 100, 7: 100, 12: 86,
+#     36: 100, 25: 100, 5: 100, 14: 86, 39: 20, 38: 20, 27: 20, 9: 20, 34: 20, 15: 20, 3: 20, 6: 20, 13: 20, 19: 20, 24: 20, 11: 20, 18: 20, 29: 20, 31: 20, 10: 20, 32: 20}
