f120b244dacad04e066ace8ae91f2a122cac7304
Ethan707 <24852017+Ethan707@users.noreply.github.com>
2022-04-15 01:36:48 -0600

    separate train and evaluation

--- src/loss.py
+++ src/loss.py
@@ -1,7 +1,7 @@
 '''
 Author: Ethan Chen
 Date: 2022-04-08 07:37:13
-LastEditTime: 2022-04-12 22:17:44
+LastEditTime: 2022-04-15 01:31:45
 LastEditors: Ethan Chen
 Description: 
 FilePath: /CMPUT414/src/loss.py
@@ -19,6 +19,42 @@ import torch
 # pointnet pytorch
 # https://github.com/fxia22/pointnet.pytorch
 # https://arxiv.org/abs/1612.00593
+
+
+def knn(x, k):
+    inner = -2*torch.matmul(x.transpose(2, 1), x)
+    xx = torch.sum(x**2, dim=1, keepdim=True)
+    pairwise_distance = -xx - inner - xx.transpose(2, 1)
+
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]   # (batch_size, num_points, k)
+    return idx
+
+
+def get_graph_feature(x, k=20, idx=None):
+    batch_size = x.size(0)
+    num_points = x.size(2)
+    x = x.view(batch_size, -1, num_points)
+    if idx is None:
+        idx = knn(x, k=k)   # (batch_size, num_points, k)
+    device = torch.device('cuda')
+
+    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
+
+    idx = idx + idx_base
+
+    idx = idx.view(-1)
+
+    _, num_dims, _ = x.size()
+
+    # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
+    x = x.transpose(2, 1).contiguous()
+    feature = x.view(batch_size*num_points, -1)[idx, :]
+    feature = feature.view(batch_size, num_points, k, num_dims)
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+
+    feature = torch.cat((feature-x, x), dim=3).permute(0, 3, 1, 2).contiguous()
+
+    return feature
 
 
 class STN3d(nn.Module):
@@ -125,14 +161,84 @@ class PointNetCls(nn.Module):
         return F.log_softmax(x, dim=1), trans, trans_feat, self.feature
 
 
-def feature_transform_regularizer(trans):
-    d = trans.size()[1]
-    batchsize = trans.size()[0]
-    I = torch.eye(d)[None, :, :]
-    if trans.is_cuda:
-        I = I.cuda()
-    loss = torch.mean(torch.norm(torch.bmm(trans, trans.transpose(2, 1)) - I, dim=(1, 2)))
-    return loss
+class DGCNN(nn.Module):
+    def __init__(self, k, emb_dims, dropout, output_channels=40):
+        super(DGCNN, self).__init__()
+        # self.args = args
+        self.k = k
+
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm1d(emb_dims)
+
+        self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=1, bias=False),
+                                   self.bn1,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv2 = nn.Sequential(nn.Conv2d(64*2, 64, kernel_size=1, bias=False),
+                                   self.bn2,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv3 = nn.Sequential(nn.Conv2d(64*2, 128, kernel_size=1, bias=False),
+                                   self.bn3,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv4 = nn.Sequential(nn.Conv2d(128*2, 256, kernel_size=1, bias=False),
+                                   self.bn4,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.conv5 = nn.Sequential(nn.Conv1d(512, emb_dims, kernel_size=1, bias=False),
+                                   self.bn5,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.linear1 = nn.Linear(emb_dims*2, 512, bias=False)
+        self.bn6 = nn.BatchNorm1d(512)
+        self.dp1 = nn.Dropout(p=dropout)
+        self.linear2 = nn.Linear(512, 256)
+        self.bn7 = nn.BatchNorm1d(256)
+        self.dp2 = nn.Dropout(p=dropout)
+        self.linear3 = nn.Linear(256, output_channels)
+
+    def forward(self, x):
+        # print(x.shape)
+        self.feature = []
+        x = x.transpose(2, 1)
+        batch_size = x.size(0)
+        x = get_graph_feature(x, k=self.k)
+        self.feature.append(x)
+        x = self.conv1(x)
+        self.feature.append(x)
+        x1 = x.max(dim=-1, keepdim=False)[0]
+
+        x = get_graph_feature(x1, k=self.k)
+        self.feature.append(x)
+        x = self.conv2(x)
+        self.feature.append(x)
+        x2 = x.max(dim=-1, keepdim=False)[0]
+
+        x = get_graph_feature(x2, k=self.k)
+        self.feature.append(x)
+        x = self.conv3(x)
+        self.feature.append(x)
+        x3 = x.max(dim=-1, keepdim=False)[0]
+
+        x = get_graph_feature(x3, k=self.k)
+        self.feature.append(x)
+        x = self.conv4(x)
+        self.feature.append(x)
+        x4 = x.max(dim=-1, keepdim=False)[0]
+
+        x = torch.cat((x1, x2, x3, x4), dim=1)
+
+        x = self.conv5(x)
+        self.feature.append(x)
+        x1 = F.adaptive_max_pool1d(x, 1).view(batch_size, -1)
+        x2 = F.adaptive_avg_pool1d(x, 1).view(batch_size, -1)
+        x = torch.cat((x1, x2), 1)
+
+        x = F.leaky_relu(self.bn6(self.linear1(x)), negative_slope=0.2)
+        x = self.dp1(x)
+        x = F.leaky_relu(self.bn7(self.linear2(x)), negative_slope=0.2)
+        x = self.dp2(x)
+        x = self.linear3(x)
+        return x, self.feature
 
 
 class PointNetLoss(nn.Module):
@@ -150,6 +256,23 @@ class PointNetLoss(nn.Module):
         loss = 0.0
         for i in range(len(feature_map)):
             loss += F.smooth_l1_loss(feature_map[i], feature_map_2[i])
+        return loss
+
+
+class DGCNNLoss(nn.Module):
+    def __init__(self, model, device, alpha):
+        super(DGCNNLoss, self).__init__()
+        self.model = model.eval()
+        self.device = device
+        for parm in self.model.parameters():
+            parm.requires_grad = False
+
+    def forward(self, pc1, pc2):
+        _, _, _, feature_map = self.model(pc1.to(self.device))
+        _, _, _, feature_map_2 = self.model(pc2.to(self.device))
+        loss = 0.0
+        for i in range(len(feature_map)):
+            loss += F.l1_loss(feature_map[i], feature_map_2[i])
         return loss
 
 
